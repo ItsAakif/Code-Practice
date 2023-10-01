9bcabe0b261657146957679bf6a38c20d941932a
Moutasim <moutasimqazi@gmail.com>
2023-10-01 11:35:21 +0530

    3 Python File With Different Differen Codes

---
+++ Python/Open_Cv.py
@@ -0,0 +1,47 @@
+import cv2
+
+# Load the pre-trained Haar Cascade Classifier for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def blur_faces(frame):
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+    # Iterate over the detected faces and apply Gaussian blur to each face region
+    for (x, y, w, h) in faces:
+        face_roi = frame[y:y+h, x:x+w]
+        face_roi = cv2.GaussianBlur(face_roi, (99, 99), 30)
+
+        # Replace the original face region with the blurred one
+        frame[y:y+h, x:x+w] = face_roi
+
+    return frame
+
+def main():
+    # Open the webcam
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read a frame from the webcam
+        ret, frame = cap.read()
+
+        # Apply face detection and blurring
+        frame = blur_faces(frame)
+
+        # Display the result in a window
+        cv2.imshow("Face Blur", frame)
+
+        # Break the loop when the user presses the 'q' key
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close all OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+# <Open_CV.py> by Moutasim Qazi
