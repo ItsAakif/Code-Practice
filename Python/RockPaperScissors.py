b731a713cfedd7de1450f08773807d781ca83f95
lordgrim18 <jeromjomanthara@gmail.com>
2023-10-10 02:42:16 +0530

    updating rock-paper-scissors

--- Python/RockPaperScissors.py
+++ Python/RockPaperScissors.py
@@ -1,4 +1,5 @@
 import random
+import time 
 
 def get_user_choice():
     while True:
@@ -12,25 +13,30 @@ def get_computer_choice():
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return "It's a tie!"
+        return "\nIt's a tie!"
     elif (user_choice == 'rock' and computer_choice == 'scissors') or \
          (user_choice == 'paper' and computer_choice == 'rock') or \
          (user_choice == 'scissors' and computer_choice == 'paper'):
-        return "You win!"
+        return "\nYou win!"
     else:
-        return "Computer wins!"
+        return "\nComputer wins!"
 
 def play_game():
     while True:
         print("Let's play Rock, Paper, Scissors!")
+        print("****************************************")
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
-        print(f"You chose {user_choice}.")
+        time.sleep(1)
+        print(f"\nYou chose {user_choice}.")
+        time.sleep(1)
         print(f"Computer chose {computer_choice}.")
+        time.sleep(1)
         result = determine_winner(user_choice, computer_choice)
         print(result)
+        time.sleep(1)
         while True:
-            play_again = input("Play again? (y/n): ").strip().lower()
+            play_again = input("\nPlay again? (y/n): ").strip().lower()
             if play_again == 'n':
                 return  # Exit the game loop
             elif play_again == 'y':
