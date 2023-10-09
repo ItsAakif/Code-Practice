9454564cb72ffdb359e9dbf815847bf830a568f2
lordgrim18 <jeromjomanthara@gmail.com>
2023-10-10 02:44:39 +0530

    adding comments to rock-paper-scissors

--- Python/RockPaperScissors.py
+++ Python/RockPaperScissors.py
@@ -1,6 +1,7 @@
 import random
-import time 
+import time
 
+# Function to get the user's choice with input validation
 def get_user_choice():
     while True:
         user_choice = input("Choose Rock, Paper, or Scissors: ").strip().lower()
@@ -8,9 +9,11 @@ def get_user_choice():
             return user_choice
         print("Invalid choice. Please choose Rock, Paper, or Scissors.")
 
+# Function to get the computer's choice
 def get_computer_choice():
     return random.choice(['rock', 'paper', 'scissors'])
 
+# Function to determine the winner of the game
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         return "\nIt's a tie!"
@@ -21,13 +24,20 @@ def determine_winner(user_choice, computer_choice):
     else:
         return "\nComputer wins!"
 
+# Main game loop
 def play_game():
     while True:
         print("Let's play Rock, Paper, Scissors!")
         print("****************************************")
+        
+        # Introduce delays for a more user-friendly experience
+        time.sleep(1)
+
+        # Get user and computer choices
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
-        time.sleep(1)
+        
+        # Display choices and determine the winner
         print(f"\nYou chose {user_choice}.")
         time.sleep(1)
         print(f"Computer chose {computer_choice}.")
@@ -35,6 +45,8 @@ def play_game():
         result = determine_winner(user_choice, computer_choice)
         print(result)
         time.sleep(1)
+        
+        # Ask the user if they want to play again
         while True:
             play_again = input("\nPlay again? (y/n): ").strip().lower()
             if play_again == 'n':
@@ -43,7 +55,6 @@ def play_game():
                 break  # Continue to the next game
             else:
                 print("Invalid input. Please enter 'y' or 'n'.")
-
 
 if __name__ == '__main__':
     play_game()
