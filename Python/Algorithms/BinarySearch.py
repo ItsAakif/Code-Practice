0facf6b42b214e8fb199e4332d8bf187ad36d85e
lordgrim18 <jeromjomanthara@gmail.com>
2023-10-14 12:20:54 +0530

    cleaning binary search

--- Python/Algorithms/BinarySearch.py
+++ Python/Algorithms/BinarySearch.py
@@ -1,6 +1,6 @@
+# Binary search function
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
-
     while left <= right:
         mid = (left + right) // 2
         if arr[mid] == target:
@@ -9,15 +9,19 @@ def binary_search(arr, target):
             left = mid + 1
         else:
             right = mid - 1
-    return -1 
+    return -1  # Target not found in the array
 
+# Obtain a sorted list from the user
 input_str = input("Enter a sorted list of space-separated values: ")
 arr = [int(x) for x in input_str.split()]
 
+# Get the target value from the user
 target = int(input("Enter the value to search for: "))
 
+# Perform binary search
 result = binary_search(arr, target)
 
+# Check and display the result
 if result != -1:
     print(f"Element {target} found at index {result}")
 else:
