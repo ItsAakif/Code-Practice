0deafd4a584b14563ecd75435b9b1be036f21ad1
Bitan Kundu <115944988+BitanKundu1@users.noreply.github.com>
2023-10-16 01:28:03 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void selectionSort(int *A, int n){
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
+
+int main(){
+    int A[] = {3, 5, 2, 13, 12};
+    int n = 5;
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
+
+    return 0;
+}
