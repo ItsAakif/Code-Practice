8bfbe4ea5828bc29923fd90814da9b1b87a20147
lordgrim18 <jeromjomanthara@gmail.com>
2023-10-15 14:15:20 +0530

    updated hangman - added pauses at appropriate locations

--- Python/Projects/Hangman.py
+++ Python/Projects/Hangman.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 categories = {
     "Animals": ["elephant", "giraffe", "kangaroo", "octopus", "rhinoceros"],
@@ -26,16 +27,19 @@ def hangman():
     score = 0
 
     print(f"Category: {category}")
-    print("Welcome to Hangman!")
+    print("\nWelcome to Hangman!")
 
     while True:
         displayed_word = display_word(word, guessed_letters)
+        time.sleep(0.1)  # Pause for 0.1 seconds
         print(displayed_word)
         print(f"Attempts left: {attempts}")
 
         if displayed_word == word:
             score += 1
+            time.sleep(1)  # Pause for 1 second
             print(f"Congratulations! You guessed the word '{word}'")
+            time.sleep(1)  # Pause for 1 second
             play_again = input("Play again? (yes/no): ").lower()
             if play_again != "yes":
                 break
@@ -45,9 +49,12 @@ def hangman():
                 attempts = 6
                 print(f"Category: {category}")
                 print("New round!")
+                time.sleep(1)  # Pause for 1 second
 
         if attempts == 0:
-            print(f"Game over! The word was '{word}'. Your score: {score}")
+            time.sleep(1) # Pause for 1 second
+            print(f"Game over! The word was '{word}'")
+            time.sleep(1)  # Pause for 1 second
             play_again = input("Play again? (yes/no): ").lower()
             if play_again != "yes":
                 break
@@ -57,6 +64,7 @@ def hangman():
                 attempts = 6
                 print(f"Category: {category}")
                 print("New round!")
+                time.sleep(1)  # Pause for 1 second
 
         guess = input("Guess a letter: ").lower()
 
@@ -71,6 +79,7 @@ def hangman():
         else:
             print("Wrong guess!")
             attempts -= 1
+        time.sleep(0.5)  # Pause for 0.5 seconds
 
 if __name__ == "__main__":
     hangman()
