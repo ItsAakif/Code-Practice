0e7cf602d87ff23da6ff60a1bdf33cf71768bc63
Abhishek Kumar <121042725+AbhishekKumar993@users.noreply.github.com>
2023-10-28 04:13:58 +0530

    Create Rat_In_A_Maze.cpp
    solved using Backtracking.

---
+++ C++/Algorithm/Rat_In_A_Maze.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Maze size
+#define N 4
+
+bool solveMazeUtil(int maze[N][N], int x, int y,int sol[N][N]);
+
+
+void printSolution(int sol[N][N])
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cout<<" "<<sol[i][j]<<" ";
+		cout<<endl;
+	}
+}
+
+
+// N*N maze
+bool isSafe(int maze[N][N], int x, int y)
+{
+	// if (x, y outside maze) return false
+	if (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1)
+		return true;
+	return false;
+}
+
+
+bool solveMaze(int maze[N][N])
+{
+	int sol[N][N] = { { 0, 0, 0, 0 },
+					{ 0, 0, 0, 0 },
+					{ 0, 0, 0, 0 },
+					{ 0, 0, 0, 0 } };
+	if (solveMazeUtil(maze, 0, 0, sol) == false) {
+		cout<<"Solution doesn't exist";
+		return false;
+	}
+	printSolution(sol);
+	return true;
+}
+
+
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N])
+{
+	if (x == N - 1 && y == N - 1 && maze[x][y] == 1) {
+		sol[x][y] = 1;
+		return true;
+	}
+	// Check if maze[x][y] is valid
+	if (isSafe(maze, x, y) == true) {
+    if (sol[x][y] == 1)
+			return false;
+		sol[x][y] = 1;
+		if (solveMazeUtil(maze, x + 1, y, sol) == true)
+			return true;
+		if (solveMazeUtil(maze, x - 1, y, sol) == true)
+			return true;
+		if (solveMazeUtil(maze, x, y + 1, sol) == true)
+			return true;
+		if (solveMazeUtil(maze, x, y - 1, sol) == true)
+			return true;
+		// If none of the above movements work then
+		// BACKTRACK: unmark x, y as part of solution path
+		sol[x][y] = 0;
+		return false;
+	}
+	return false;
+}
+
+int main()
+{
+	int maze[N][N] = { { 1, 0, 0, 0 },
+					{ 1, 1, 0, 1 },
+					{ 0, 1, 0, 0 },
+					{ 1, 1, 1, 1 } };
+	solveMaze(maze);
+	return 0;
+}
+
+// This code is contributed by Aditya Kumar (adityakumar129)
+// Changes made for N>4 by Ch. Abdul Wasay
